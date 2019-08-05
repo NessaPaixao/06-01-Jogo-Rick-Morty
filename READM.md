881dc5de9417c21332199517f597fbb2fadecdf8
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-08-05 17:46:43 -0300

    Adicionar javascript e css

---
+++ READM.md
@@ -0,0 +1,30 @@
+##Jogo Rick and Morty Quizz
+
+#Linguagens utilizadas
+
+* HTML
+* CSS
+* JAVASCRIPT 
+
+#Ferramentas das linguagens
+
+* Utilizamos em CSS display Flex
+* Utilizamos em Script, Function e Variáveis
+
+#Como Jogar
+
+* Clique no botão iniciar, em seguida aparecerá uma imagem de um personagem, digite o nome correto do personagem na caixa de texto e clique em validar. Faça sua pontuação e se divirta!
+
+#API Utilizada 
+
+* https://rickandmortyapi.com/api/character/
+
+#Integrantes do grupo
+
+* Cristian Rafael
+* Priscila Araújo
+* Thiago Mendes
+* Vanessa da Paixao
+
+
+* Para jogar, [clique aqui](https://nessapaixao.github.io/06-01-Jogo-Rick-Morty/)
--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let caixaTexto = document.querySelector("input");
 let iniciar = document.querySelector("#iniciar");
 let tentar = document.querySelector("#tentar");
-let pontuacao = document.querySelector("#pontuacao");
-let foto = document.querySelector ("img")
+let pontuacao = document.querySelector(".pontuacao");
+let foto = document.querySelector("img");
+let pontos = document.querySelector("#pontos");
+let contador = 0;
 
+
+
+pontos.innerHTML = contador;
 pesquisarPersonagem = () =>{
     let aleatorio = Math.floor(Math.random()*493);
     fetch(`https://rickandmortyapi.com/api/character/${aleatorio}`)
@@ -13,21 +18,26 @@ pesquisarPersonagem = () =>{
     .then(function(data){
         foto.src = data.image;
         console.log(data.name);
-        let nomeDoPersonagem = data.name.toUpperCase().toString();
-        tentar.onclick = function(){
+        
+        verificar=()=>{
             let input = caixaTexto.value.toUpperCase().toString();
-            if (nomeDoPersonagem == input){
-                alert ("voce acertou!")
+            let nomeDoPersonagem = data.name.toUpperCase().toString();
+
+            console.log(input)
+            if (caixaTexto !== "" && nomeDoPersonagem === input){
+                contador ++;
+                pontos.innerHTML = contador;
+                pesquisarPersonagem();
             } else{
-                alert ("vc errou")
-            }
-        };
+                pontos.innerHTML = "Poxa, você perdeu! Tente novamente"
+                pesquisarPersonagem();
+            } 
+            caixaTexto.value = "" ; 
+        }
+        tentar.onclick = verificar;
     })
 }    
 
-function pontuacao(){
-    if (input = true) contador >>
-    }
 
 
 iniciar.onclick = pesquisarPersonagem;
